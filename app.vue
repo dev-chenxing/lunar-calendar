ec59d1a3bd9dbb668af579d01ba6fadeaa091538
陈刑 <chenxing.io@outlook.com>
2024-12-29 13:14:57 +0800

    nextJieQi

--- app.vue
+++ app.vue
@@ -8,17 +8,15 @@ const monthInChinese = now.toLocaleString('zh', { month: 'long' })
 const monthDayCount = SolarMonth.fromDate(now).getDays().length
 const monthType = month == 2 ? "平" : (monthDayCount == 31 ? "大" : "小")
 const date = now.getDate()
-const weekday = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"][now.getDay()]
-const hours = now.getHours()
-const minutes = now.getMinutes().toString().padStart(2, "0")
-const greetings = hours < 12 ? "早安" : (hours < 18 ? "午安" : "晚安")
-const greetingsColor = hours < 12 ? "text-yellow-300" : (hours < 18 ? "text-orange-200" : "text-blue-300")
+const weekday = now.toLocaleString('zh', { weekday: 'long' })
 
 const lunarDate = Lunar.fromDate(now)
 const lunarYear = lunarDate.getYear()
 const lunarYearInChinese = lunarDate.getYearInChinese()
 const lunarMonth = lunarDate.getMonth()
-const lunarDay = lunarDate.getJieQi() || `${lunarDate.getDayInChinese()}日`
+const lunarDay = `${lunarDate.getDayInChinese()}日`
+const lunarJieQi = lunarDate.getJieQi()
+const lunarNextJieQi = lunarDate.getNextJieQi()
 const lunarMonthDayCount = LunarMonth.fromYm(lunarYear, lunarMonth)?.getDayCount()
 </script>
 
@@ -32,22 +30,22 @@ const lunarMonthDayCount = LunarMonth.fromYm(lunarYear, lunarMonth)?.getDayCount
         class="absolute border-2 border-green-600 rounded left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white min-w-16 min-h-[36px]">
         <div
           class="absolute inline border-2 border-green-600 rounded h-6 w-20 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white leading-[0.675] text-[1.725rem] overflow-visible -z-20">
-          <div class="absolute bg-white w-[60px] h-[30px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center -z-10"></div>
+          <div
+            class="absolute bg-white w-[60px] h-[30px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center -z-10">
+          </div>
           <span>{{ year }}</span>
         </div>
       </div>
     </div>
-    <div class="border-2 border-green-600 text-text-2xl p-0.5">
-      <div class="grid grid-cols-[1fr_auto_1fr] text-5xl *:flex *:items-center *:justify-center">
-        <div class="[writing-mode:vertical-lr]">人間忽晚</div>
-        <div class="font-bold text-[18rem] leading-none drop-shadow-green">{{ date }}</div>
-        <div class="[writing-mode:vertical-lr]">山河以秋</div>
-      </div>
-      <div class="grid grid-cols-3 *:text-center">
-        <div>{{ lunarYearInChinese }}</div>
-        <div class="font-bold">{{ lunarDay }}</div>
-        <div>{{ lunarMonth }}</div>
-      </div>
+    <div class="flex justify-between p-1 *:flex *:items-center *:justify-center">
+      <div class="[writing-mode:vertical-lr]">人間忽晚</div>
+      <div class="font-bold text-[18rem] leading-none drop-shadow-green">{{ date }}</div>
+      <div class="[writing-mode:vertical-lr]">山河以秋</div>
+    </div>
+    <div class="grid grid-cols-3 *:text-center">
+      <div class="font-sans-serif font-bold">{{ lunarDay }}</div>
+      <div>{{ lunarJieQi || lunarNextJieQi.getSolar().getLunar().getDayInChinese() + lunarNextJieQi.getName() }}</div>
+      <div class="font-sans-serif font-bold">{{ weekday }}</div>
     </div>
   </main>
 </template>
