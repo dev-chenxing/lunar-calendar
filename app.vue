d42e8e5492a0c99033e693131c0963d8b89d6cf4
陈刑 <chenxing.io@outlook.com>
2025-01-05 23:47:02 +0800

    weekend red

--- app.vue
+++ app.vue
@@ -4,8 +4,6 @@ import Favicon from "./components/Favicon.vue"
 import { Lunar, LunarMonth, Solar, SolarMonth } from "lunar-typescript"
 import { tify } from 'chinese-conv'
 
-const color = "#267F00"
-
 const CHINESE_NUMBERS = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"]
 const commonYiJisPriorty: Record<string, number> = { "开市": 3, "安门": 1, "修造": 3, "动土": 3, "竖柱": 3, "上梁": 2, "祭祀": 3, "嫁娶": 3, "栽种": 2, "理发": 2, "伐木": 2, "畋猎": 1, "酝酿": 1, "安床": 2, "架马": 1, "祈福": 3, "会友": 2, "入学": 1, "裁衣": 2, "交易": 1, "出行": 3, "破土": 2, "安葬": 2, "沐浴": 2, "扫舍": 1, "捕捉": 1, "诸事不宜": 1, "破屋": 1, "坏垣": 1, "求医": 1, "治病": 2, "求医疗病": 1, "开仓": 2, "探病": 1, "取渔": 1, "作灶": 2, "解除": 2, "成服": 1, "词讼": 1, "移徙": 1, "会亲友": 1, "修饰垣墙": 1, "平治道涂": 1, "无": 1 }
 const twoWordYiJiMap: Record<string, string> = {
@@ -18,19 +16,20 @@ const SHENG_XIAO_SAN_HE = ["鼠龙猴", "牛蛇鸡", "虎马狗", "兔猪羊"]
 
 
 
-const now = new Date(2025, 0, 5)
+const now = new Date(2025, 1, 1)
 const year = now.getFullYear()
 const month = now.getMonth() + 1
 const date = now.getDate()
-
 const numOfDay = Solar.fromDate(now).subtract(Solar.fromYmd(year, 1, 1))
 const today = new Date(year, month - 1, date)
 const monthInEnglish = today.toLocaleString('en', { month: 'long' }).toUpperCase()
 const monthInChinese = today.toLocaleString('zh', { month: 'long' })
 const monthDayCount = SolarMonth.fromDate(today).getDays().length
 const monthType = month == 2 ? "平" : (monthDayCount == 31 ? "大" : "小")
-const weekday = today.toLocaleString('en', { weekday: 'long' })
+const weekday = today.getDay()
+const weekdayInEnglish = today.toLocaleString('en', { weekday: 'long' })
 const weekdayInChinese = today.toLocaleString('zh', { weekday: 'long' })
+const color = (weekday == 0 || weekday == 6) ? "#C42D15" : "#267F00"
 
 const lunarDate = Lunar.fromDate(today)
 const lunarYear = lunarDate.getYear()
@@ -73,7 +72,7 @@ onMounted(() => {
 
 <template>
 	<Favicon :date="date" :color="color" />
-	<main class="gap-2 py-2 text-2xl text-green" ref="main">
+	<main class="gap-2 py-2 text-2xl" ref="main" :style="{ color: color }">
 		<div class="relative border-2 text-center grid grid-cols-3 *:leading-tight">
 			<div>{{ monthInEnglish }}</div>
 			<div></div>
@@ -90,14 +89,14 @@ onMounted(() => {
 			</div>
 		</div>
 		<div class="grid grid-cols-3 p-1 *:items-center *:justify-center h-[14.5rem] md:h-[19rem]">
-			<div class="[writing-mode:vertical-rl] mr-auto text-sm">
+			<div class="[writing-mode:vertical-rl] mr-auto text-sm text-center">
 				<ChinesePoetry :day="numOfDay" line="2" />
 			</div>
-			<div
-				class="flex font-bold text-[15rem] md:text-[18rem] h-56 md:h-auto leading-none drop-shadow-green text-center">
-				{{ date
-				}}</div>
-			<div class="[writing-mode:vertical-lr] ml-auto text-sm">
+			<div class="flex font-bold text-[15rem] md:text-[18rem] h-56 md:h-auto leading-none text-center"
+				:style="{ textShadow: `3px 2px 0 white, 6px 4px 0 ${color}` }">
+				{{ date }}
+			</div>
+			<div class="[writing-mode:vertical-lr] ml-auto text-sm text-center">
 				<ChinesePoetry :day="numOfDay" line="1" />
 			</div>
 		</div>
@@ -112,19 +111,19 @@ onMounted(() => {
 				<p v-if="lunarShuJiu">
 					{{ lunarShuJiu.getName() }}第{{ CHINESE_NUMBERS[lunarShuJiu.getIndex()] }}天
 				</p>
-				<p>{{ lunarJieQi || lunarNextJieQi.getSolar().getLunar().getDayInChinese() +
-					lunarNextJieQi.getName() }}</p>
+				<p>{{ lunarJieQi || (lunarNextJieQi.getSolar().getLunar().getDayInChinese() + lunarNextJieQi.getName())
+					}}</p>
 			</div>
 			<div class="border-l-2">
-				<p class="text-sm h-[20px]">{{ weekday }}</p>
+				<p class="text-sm h-[20px]">{{ weekdayInEnglish }}</p>
 				<p class="font-sans-serif font-bold text-3xl pb-1">{{ weekdayInChinese }}</p>
 			</div>
 		</div>
 
 		<div class="flex flex-wrap border-x-2 border-b-2 text-base *:border-x *:border-b">
 			<div class="flex flex-auto items-center justify-center min-w-48">
-				<span
-					class="inline-block bg-green text-white rounded-full h-8 w-8 text-center m-1 text-xl font-sans-serif">宜</span>
+				<span class="inline-block text-white rounded-full h-8 w-8 text-center m-1 text-xl font-sans-serif"
+					:style="{ backgroundColor: color }">宜</span>
 				<span ref="lunarYi">{{
 					tify(lunarDate.getDayYi()
 						.filter(isCommonYiJi)
@@ -149,8 +148,8 @@ onMounted(() => {
 				</div>
 			</div>
 			<div class="flex flex-auto items-center min-w-48 justify-center">
-				<span
-					class="inline-block bg-green text-white rounded-full h-8 w-8 text-center m-1 text-xl font-sans-serif">忌</span>
+				<span class="inline-block text-white rounded-full h-8 w-8 text-center m-1 text-xl font-sans-serif"
+					:style="{ backgroundColor: color }">忌</span>
 				<span ref="lunarJi">{{ tify(lunarDate.getDayJi()
 					.filter(isCommonYiJi)
 					.map((yiji) => twoWordYiJiMap[yiji] ? twoWordYiJiMap[yiji] : yiji)
@@ -158,9 +157,10 @@ onMounted(() => {
 					.slice(0, 4)
 					.join(" ")) }}</span>
 			</div>
-			<div class="min-w-20 flex flex-auto items-center justify-center *:[writing-mode:vertical-rl]">
+			<div class="min-w-24 flex flex-auto items-center justify-center *:[writing-mode:vertical-rl]">
 				<span
-					class="bg-green text-white text-sm rounded-xl m-1 py-[.25rem] font-sans-serif min-w-[1.35rem] leading-tight tracking-[.1em]">
+					class="text-white text-sm rounded-xl m-1 py-[.25rem] font-sans-serif min-w-[1.35rem] leading-tight tracking-[.1em]"
+					:style="{ backgroundColor: color }">
 					今日胎神
 				</span>
 				<span v-for="positionTai in lunarPositionTai" class="text-xl tracking-widest w-fit">
@@ -168,7 +168,8 @@ onMounted(() => {
 				</span>
 			</div>
 			<div class="flex flex-auto flex-col min-w-14">
-				<span class="bg-green text-white text-xs rounded-xl m-1 text-center font-sans-serif tracking-wide">
+				<span class="text-white text-xs rounded-xl m-1 text-center font-sans-serif tracking-wide"
+					:style="{ backgroundColor: color }">
 					是日
 				</span>
 				<div class="flex justify-center pb-1">
@@ -201,7 +202,8 @@ onMounted(() => {
 			</div>
 			<div class="flex flex-auto items-center justify-center min-w-20">
 				<span
-					class="bg-green text-white text-sm rounded-xl mr-2 py-[.25rem] font-sans-serif w-[1.35rem] leading-tight tracking-[.1em] [writing-mode:vertical-rl]">
+					class="text-white text-sm rounded-xl mr-2 py-[.25rem] font-sans-serif w-[1.35rem] leading-tight tracking-[.1em] [writing-mode:vertical-rl]"
+					:style="{ backgroundColor: color }">
 					今日八字
 				</span>
 				<div>
@@ -211,7 +213,8 @@ onMounted(() => {
 				</div>
 			</div>
 			<div class="flex flex-auto flex-col min-w-24">
-				<span class="bg-green text-white text-xs rounded-xl m-1 text-center font-sans-serif tracking-wide">
+				<span class="text-white text-xs rounded-xl m-1 text-center font-sans-serif tracking-wide"
+					:style="{ backgroundColor: color }">
 					吉神方位
 				</span>
 				<div class="flex justify-center pb-1">
@@ -237,7 +240,7 @@ onMounted(() => {
 						sanhe.includes(lunarDayShengXiao))?.replace(lunarDayShengXiao, "")) }}
 				</span>
 				<span class="text-sm font-sans-serif">
-					{{ lunarDayShengXiao }}日衝{{ lunarDate.getDayChongShengXiao() }}生年{{
+					{{ tify(lunarDayShengXiao) }}日衝{{ tify(lunarDate.getDayChongShengXiao()) }}生年{{
 						shengXiaoAge[lunarDate.getDayChongShengXiao()] }}歲
 				</span>
 			</div>
